34087109e7f119b13969e82035c9c56f7c64ddad
leandro <leandro.agustin20@outlook.com>
2025-02-05 18:41:48 -0300

    routes and heroCards created

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { getHeroeByPublisher } from '../helpers'
+import { HeroCard } from './HeroCard'
+
+export const HeroList = ({ publisher } ) => {
+    const heroes = getHeroeByPublisher(publisher)
+  return (
+    <>
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
+            { heroes.map(heroe => (
+                <HeroCard key={heroe.id} {...heroe} />
+            )) }
+        </div>
+    </>
+  )
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
-import { DcPage } from '../heroes/pages/DcPage'
-import { LoginPage } from '../auth/pages/LoginPage'
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { HeroesRouter } from '../heroes'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path='marvel' element={<MarvelPage />} />
-            <Route path='dc' element={<DcPage />} />
-
             <Route path='login' element={<LoginPage />} />
-            <Route path='/' element={<Navigate to="/marvel" />} />
+            <Route path='/*' element={<HeroesRouter />} />
         </Routes>
     </>
   )
