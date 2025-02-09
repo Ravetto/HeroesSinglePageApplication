8f16df173dce937430c6970d86da4ac6b420a4dc
leandro <leandro.agustin20@outlook.com>
2025-02-09 13:22:31 -0300

    userLogin && userLogout + localStorage

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -4,13 +4,16 @@ import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
-    const { user } = useContext(AuthContext)
+    const { user, logout } = useContext(AuthContext)
+    console.log(user)
     const navigate = useNavigate()
     const onLogout = () => {
+        logout()
         navigate('/login', {
             replace: true
         })
     }
+ 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -50,7 +53,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-primary'>{user.name}</span>
+                    <span className='nav-item nav-link text-primary'>{user?.name}</span>
                     <button onClick={onLogout} className='nav-item nav-link btn'>logout</button>
                 </ul>
             </div>
